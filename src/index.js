3b2a380c493c338b0065245187de619b31086be9
Ankit Upadhyay <uankit53@outlook.com>
2021-04-25 10:45:33 +0530

    Created Basic Layout

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
-import "./index.css";
-import App from "./App";
+import "./css/index.css";
+import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/js/bootstrap";
 import "jquery/dist/jquery";
